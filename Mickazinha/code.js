0a6c6e8fc03f57e740177cef141cda974c1548f0
butterflySZaf <134169827+butterflySZaf@users.noreply.github.com>
2023-08-29 00:08:31 -0300

    Update code.js

--- Mickazinha/code.js
+++ Mickazinha/code.js
@@ -1,9 +1,4 @@
 document.getElementById("startButton").addEventListener("click", function() {
-    // Carregar e exibir a página "startpage.html" aqui
-    fetch("Pages/startpage.html")
-        .then(response => response.text())
-        .then(content => {
-            document.body.innerHTML = content;
-        })
-        .catch(error => console.error(error));
+    // Redirecionar para a página "startpage.html"
+    window.location.href = "https://seunome.github.io/seuprojeto/Pages/startpage.html";
 });
